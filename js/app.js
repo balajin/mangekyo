a104e7b0b7d17a19cf0e35074e5f0b631544ba72
Balaji Narain <narainbalaji@gmail.com>
2014-03-28 22:04:01 +0530

    BalajiN | Adding support for menus

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-var app = angular.module("Mangekyou", []);
+window.app = angular.module("Mangekyou", []);
 
-app.config(['$httpProvider', function($httpProvider) {
+window.app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
   }
 ]);
 
-app.controller('formController', ['$scope', '$http', function($scope, $http) {
+window.app.controller('formController', ['$scope', '$http', function($scope, $http) {
 	$scope.submit = function(){
 		var request = {
 			_id: $scope.userid,
---
+++ js/view.js
@@ -0,0 +1,3 @@
+window.app.controller('viewController', ['$scope', function($scope){
+	$scope.menu="entry";
+}])
