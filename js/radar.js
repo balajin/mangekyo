33d8548b4c1bb38802f9e6d6f28c600328b0e154
subramaniam <subramaniam02@gmail.com>
2014-03-29 10:16:46 +0530

    d3js code

--- js/radar.js
+++ js/radar.js
@@ -29,6 +29,33 @@ window.app.controller('radarController', ['$scope', '$http', function($scope, $h
 				support:highest.value
 			});
 		}
+                        radar_data = $scope.response.value.value;
+                        var y_axis = [];
+                        var x_axis = [];
+                        for ( var i = 0; i < radar_data.length; i++ ){
+                            x_axis.push( radar_data[i]["label"] + " " + radar_data[i]["opinion"] );
+                            y_axis.push( radar_data[i]["support"] );
+                        }
+                        var margin = {top: 20, right: 30, bottom: 30, left: 40},
+                            width = 460 - margin.left - margin.right,
+                            height = 500 - margin.top - margin.bottom;
+                        var x = d3.scale.ordinal().domain(x_axis).rangeRoundBands([0, width], .1);
+                        var y = d3.scale.linear().range([height, 0]);
+                        var chart = d3.select(".chart")
+                            .attr("width", width + margin.left + margin.right)
+                            .attr("height", height + margin.top + margin.bottom)
+                            .append("g")
+                            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                        x.domain(radar_data.map(function(d) { return d["label"] ; }));
+                        y.domain([0,d3.max(radar_data, function(d) { return d["support"]; })]);
+                        var bar = chart.selectAll('g').data(radar_data).enter().append("g").attr( "transform", function(d,i) { return "translate("+ x(d["label"]) + ",0)"; });
+                        bar.append("rect").attr("y", function(d){return y(d["support"]); } ).attr("height",function(d) { return height - y( d["support"] ); } ).attr("width", x.rangeBand()); 
+                        bar.append("text").attr("x", x.rangeBand()  ).attr("y",function(d) { return y( d["support"] ) + 3; } ).attr("dy", ".175em").text( function(d){ return d["label"] + " " + d["opinion"];}); 
+                        var xAxis = d3.svg.axis() .scale(x) .orient("bottom");
+                        var yAxis = d3.svg.axis() .scale(y) .orient("left");
+                        chart.append("g") .attr("class", "y axis") .call(yAxis);
+                        chart.append("g") .attr("class", "x axis") .attr("transform", "translate(0," + height + ")") .call(xAxis);
+
 	})
 }])
 
